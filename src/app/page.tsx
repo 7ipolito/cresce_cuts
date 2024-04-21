783873460bd75edda71136e1b04fea5afc62cbdc
Allan Hipólito <allanhipolito66@gmail.com>
2024-04-21 01:09:47 -0300

    feat: Prettier e EsLint configurados, estrutura do uploadThing configurada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-
 export default function Home() {
   return (
-  <h1 role="texto">Oi</h1>
-  );
+    <main className="align-center a flex-1 justify-center">
+      <h1 role="texto">Main</h1>
+    </main>
+  )
 }
